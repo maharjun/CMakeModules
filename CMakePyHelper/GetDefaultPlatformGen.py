899aac346feecdc9955d7887e815d42d052495aa
maharjun <arjun210493@gmail.com>
2016-05-26 04:23:04 +0530

    Added Python cmake helper module
    The changes in this commit are:
    
    1.  Added the module CMakePyHelper to CMakeModules to do a few plat-
        form independant things that cmake itself seems to be incapable
        of doing well enough.
    
    2.  Added file GetDefaultPlatformGen.py with the function:
    
        getDefaultPlatformGen
    
        which calculates the default generator for the given platform.
        This enforces the selection of 64 bit platform on Visual Studio.
    
    3.  Added gitignore file in CMakePyHelper/ to ignore python cache dir
        and files.

---
+++ CMakePyHelper/GetDefaultPlatformGen.py
@@ -0,0 +1,46 @@
+import subprocess
+import shlex
+import re
+import platform
+
+
+def getDefaultPlatformGen():
+    #
+    # This platform gets the default 64bit generator for the platform
+    GetGenCall = "cmake -G"
+
+    p = subprocess.Popen(
+            shlex.split(GetGenCall),
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            universal_newlines=True)
+    RawCMakeOutput = p.stdout.read()
+
+    GeneratorListRe = re.compile(r"^.*?Generators\n(.*)$", re.DOTALL)
+    NonGeneratorLinesRe = re.compile(r"^  \s+?.*(\n|\Z)", re.MULTILINE)
+    GenratorNamePortion = re.compile(r"^  (\S[^=]*?)(= .*)?$", re.MULTILINE)
+    GeneratorListStr = re.sub(GeneratorListRe, r"\1", RawCMakeOutput)
+    GeneratorListStr = re.sub(NonGeneratorLinesRe, r"", GeneratorListStr)
+    GeneratorListStr = re.sub(GenratorNamePortion, r"\1", GeneratorListStr)
+    GeneratorListStr = str.splitlines(GeneratorListStr)
+
+    if platform.system() == "Windows":
+        FilteredGenListStr = [Str for Str in GeneratorListStr if re.match(r"Visual Studio (11|12|14) [0-9]+ \[arch\]", Str)]
+        print("\n".join(FilteredGenListStr))
+        if not FilteredGenListStr:
+            print(
+                "\nThe cmake in Windows does not seem to have generators for Visual Studio 11, 12,"
+                "\nor 14 for 64-bit generation")
+        else:
+            FinalGenerator = '{GenName}'.format(GenName=FilteredGenListStr[0])
+            FinalGenerator = re.sub(
+                r"(Visual Studio (11|12|14) [0-9]+ )\[arch\]\s*$",
+                r"\1Win64",
+                FinalGenerator)
+    elif platform.system() == "Linux":
+        FinalGenerator = '{GenName}'.format(GenName=FilteredGenListStr[0])
+    else:
+        FinalGenerator = '{GenName}'.format(GenName=FilteredGenListStr[0])
+    
+    return FinalGenerator
---
+++ CMakePyHelper/__init__.py
@@ -0,0 +1,1 @@
+from CMakePyHelper.GetDefaultPlatformGen import getDefaultPlatformGen
