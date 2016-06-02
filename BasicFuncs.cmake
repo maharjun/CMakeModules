b6645439d055b713c0887ea40352386443bc390d
Arjun Rao <arjun210493@gmail.com>
2016-06-03 01:23:27 +0530

    Made definition of IS_DEBUG a function
    The changes in this commit are:
    
    1.  Made definition of IS_DEBUG a function in BasicFuncs.cmake
    
            set_is_debug_def
    
    2.  Made the function a part of set_target_compile_link_flagsso that
        it needn't be done separately

--- BasicFuncs.cmake
+++ BasicFuncs.cmake
@@ -1,3 +1,13 @@
+function(set_is_debug_def target_name)
+    target_compile_definitions(
+        ${target_name}
+        PRIVATE
+        $<$<CONFIG:Debug>:IS_DEBUG=1>
+        $<$<CONFIG:Release>:IS_DEBUG=0>
+        $<$<CONFIG:RelWithDebInfo>:IS_DEBUG=0>
+        $<$<CONFIG:RelWithDebInfo>:IS_DEBUG=0>)
+endfunction()
+
 function(set_target_compile_link_flags target_name)
     if(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
         set(LTO_COMPILE_SWITCH "-GL")
@@ -19,4 +29,5 @@ function(set_target_compile_link_flags target_name)
     set(LTO_OPTION $<$<CONFIG:Release>:${LTO_LINK_SWITCH}>)
     message(STATUS "LTO_OPTION = ${LTO_OPTION}")
     target_link_libraries (${target_name} PRIVATE $<$<CONFIG:Release>:${LTO_LINK_SWITCH}>)
+    set_is_debug_def(${target_name})
 endfunction()
