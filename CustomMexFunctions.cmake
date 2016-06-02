c1434ec803b078595a7d7a3e1249f8f5e4a380bd
Arjun Rao <arjun210493@gmail.com>
2016-06-03 01:33:08 +0530

    Made Changes for addition of mex EXE interfaces - STABLE
    The changes in this commit are:
    
    1.  Added the libmat library in get_mex_libs for interfacing with MAT
        files
    
    2.  Added function install_mex_exe as a parallel to install_mex
    
    3.  Made definitions of MEX_LIB and MEX_EXE via target_compile_defin-
        itions instead of add_definitions so that they dont collide in
        case both a mex_exe and mex_lib are added
    
    4.  Added the functionality to strip _mexexe and _mexlib from the
        target names used to generate the mex exes and mex libs in order
        to enable usage of different target names and retaining the same
        output name
    
    5.  This has been tested in a Linux Platform

--- CustomMexFunctions.cmake
+++ CustomMexFunctions.cmake
@@ -6,7 +6,7 @@ function(get_mex_libs libs_var_name libdir_var_name)
         get_filename_component(Matlab_LIB_DIR ${Matlab_MEX_LIBRARY} DIRECTORY)
 
         # edit the library names and add prefix lib- if in windows
-        set(Matlab_LINK_LIBNAMES mex mx ut)
+        set(Matlab_LINK_LIBNAMES mat mex mx ut)
         if (CMAKE_SYSTEM_NAME STREQUAL "Windows")
             foreach(Matlab_lib ${Matlab_LINK_LIBNAMES})
                 list(APPEND Temp_Matlab_LINK_LIBS lib${Matlab_lib})
@@ -38,22 +38,28 @@ function(custom_add_mex_exec exec_name)
     #       LINK_TO <additional libs>
     #   )
     # 
-    # The major difference between this and the original
-    # add_mex command is the linking of the libut library
-    # , the addition of Release LTO and the addition of
-    # libraries to RPATH
+    # The major differences between this and the original add_mex command are:
+    # 
+    # 1. The stripping of any _mexexe suffix to the exec_name
+    # 2. The linking of the libut libmat and libmx library 
+    # 3. The addition of Release LTO and 
+    # 4. The addition of libraries to RPATH
 
     # parsing input arguments
     set(multiValueArgs LINK_TO)
     cmake_parse_arguments(custom_add_mex_exec "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
     
-    # add MEX_EXE definition
-    add_definitions(-DMEX_EXE)
+    # processing name of target to strip _mexexe
+    string(REGEX REPLACE "_mexexe$" "" exec_name_stripped ${exec_name})
 
     # add executable target with given source files
     add_executable(${exec_name} ${custom_add_mex_exec_UNPARSED_ARGUMENTS})
-    
-    # include directories
+    set_target_properties(${exec_name} PROPERTIES OUTPUT_NAME ${exec_name_stripped})
+
+    # add MEX_EXE definition
+    target_compile_definitions(${exec_name} PRIVATE MEX_EXE)
+
+    # include MEX include directories
     target_include_directories(${exec_name} PRIVATE ${Matlab_INCLUDE_DIRS})
     
     # link libraries
@@ -81,31 +87,53 @@ function(custom_add_mex mex_name)
     #       DOCUMENTATION <documentation file>
     #   )
     # 
-    # The major difference between this and the original
-    # add_mex command is the linking of the libut library
-    # , the addition of Release LTO and the addition of
-    # libraries to RPATH
+    # The major differences between this and the original add_mex command are:
+    # 
+    # 1. The stripping of any _mexlib suffix to the exec_name
+    # 2. Definition of MEX_LIB Definition
+    # 3. The linking of the libut libmat and libmx library 
+    # 4. The addition of Release LTO and 
+    # 5. The addition of libraries to RPATH
 
     # parse input arguments
     set(oneValueArgs DOCUMENTATION)
     set(multiValueArgs LINK_TO)
     cmake_parse_arguments(custom_add_mex "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
-    
-    # add MEX_LIB definition
-    add_definitions(-DMEX_LIB)
 
+    # processing name of target to strip _mexlib
+    string(REGEX REPLACE "_mexlib$" "" mex_name_stripped ${mex_name})
+    
+    # Get Relevant MEX Libs
     get_mex_libs(Matlab_LINK_LIBS Matlab_LIB_DIR)
+    
+    # Add Mex Lib Target
     matlab_add_mex(
         NAME ${mex_name}
         SRC ${custom_add_mex_UNPARSED_ARGUMENTS}
+        OUTPUT_NAME ${mex_name_stripped}
         LINK_TO ${Matlab_LINK_LIBS} ${custom_add_mex_LINK_TO}
         DOCUMENTATION ${custom_add_mex_DOCUMENTATION})
+
+    # add MEX_LIB definition
+    target_compile_definitions(${mex_name} PRIVATE MEX_LIB)
 
     # Set Compile and link flags
     set_target_compile_link_flags(${mex_name})
 
     # Set install rpath
     set_target_properties(${exec_name} PROPERTIES INSTALL_RPATH ${Matlab_LIB_DIR})
+endfunction()
+
+function(install_mex_exec target_name install_dir)
+    # 
+    # Function call:
+    # 
+    #   install_mex_exe(target_name, install_dir)
+    #   
+    install(TARGETS ${target_name}
+      RUNTIME DESTINATION ${install_dir}
+      CONFIGURATIONS "Release")
+    
 endfunction()
 
 function(install_mex target_name install_dir)
