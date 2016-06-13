45479c3d44d589297386d66d18c44b4701d223b0
Arjun Rao <arjun210493@gmail.com>
2016-06-14 00:51:28 +0530

    Made spacing in CppGlob.cmake consistent

--- CppGlob.cmake
+++ CppGlob.cmake
@@ -1,31 +1,31 @@
 function(GET_EXT_FILES VARIABLE DIRECTORY)
 
-	if(ARGC EQUAL 0)
-		# set nothing
-	else()
-		foreach(ext in ${ARGN})
-			file(GLOB TEMPVAR "${DIRECTORY}/*.${ext}")
-			set(EXT_FILES "${EXT_FILES};${TEMPVAR}")
-		endforeach()
-	endif()
-    set(${VARIABLE} ${EXT_FILES} PARENT_SCOPE)
-	message(STATUS "Extensions requested = ${ARGN}")
-    message(STATUS "Directory requested = ${DIRECTORY}")
+   if(ARGC EQUAL 0)
+      # set nothing
+   else()
+      foreach(ext in ${ARGN})
+         file(GLOB TEMPVAR "${DIRECTORY}/*.${ext}")
+         set(EXT_FILES "${EXT_FILES};${TEMPVAR}")
+      endforeach()
+   endif()
+   set(${VARIABLE} ${EXT_FILES} PARENT_SCOPE)
+   message(STATUS "Extensions requested = ${ARGN}")
+   message(STATUS "Directory requested = ${DIRECTORY}")
 
 endfunction()
 
 function(GET_CXX_FILES VARIABLE)
 
-	set(CXX_EXTENSIONS "cpp;c;h;hpp;inl")
+   set(CXX_EXTENSIONS "cpp;c;h;hpp;inl")
 
-    cmake_parse_arguments(get_cxx_files "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+   cmake_parse_arguments(get_cxx_files "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
-    foreach(_dir ${get_cxx_files_UNPARSED_ARGUMENTS})
-    	GET_EXT_FILES(CPP_FILES_temp ${_dir} ${CXX_EXTENSIONS})
-    	list(APPEND CPP_FILES ${CPP_FILES_temp})
-    endforeach()
-    list(REMOVE_DUPLICATES CPP_FILES)
-    
-    set(${VARIABLE} ${CPP_FILES} PARENT_SCOPE)
-    message(STATUS "Files Found = ${CPP_FILES}")
+   foreach(_dir ${get_cxx_files_UNPARSED_ARGUMENTS})
+      GET_EXT_FILES(CPP_FILES_temp ${_dir} ${CXX_EXTENSIONS})
+      list(APPEND CPP_FILES ${CPP_FILES_temp})
+   endforeach()
+   list(REMOVE_DUPLICATES CPP_FILES)
+   
+   set(${VARIABLE} ${CPP_FILES} PARENT_SCOPE)
+   message(STATUS "Files Found = ${CPP_FILES}")
 endfunction()
