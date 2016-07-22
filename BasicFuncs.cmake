220292b7c0f8e8e58cc5c8ebdb24823b80479352
Arjun Rao <arjun210493@gmail.com>
2016-07-22 18:41:36 +0530

    Removed Manual Insertion of -O3 from set_target_compile_link_flags

--- BasicFuncs.cmake
+++ BasicFuncs.cmake
@@ -18,14 +18,12 @@ function(set_target_compile_link_flags target_name)
     endif()
     if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
         set(ADDITIONAL_COMPILE_OPTIONS "-std=c++11;-march=native")
-        set(RELEASE_OPTIM_OPTION "-O3")
     endif()
     target_compile_options(${target_name} 
         PRIVATE 
         ${ADDITIONAL_COMPILE_OPTIONS}
         $<$<CONFIG:Release>:
-            ${LTO_COMPILE_SWITCH}
-            ${RELEASE_OPTIM_OPTION}>)
+            ${LTO_COMPILE_SWITCH}>)
     set(LTO_OPTION $<$<CONFIG:Release>:${LTO_LINK_SWITCH}>)
     message(STATUS "LTO_OPTION = ${LTO_OPTION}")
     target_link_libraries (${target_name} PRIVATE $<$<CONFIG:Release>:${LTO_LINK_SWITCH}>)
